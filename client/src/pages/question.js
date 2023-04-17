21b929833b8db26c10e473f40e287c01727a9051
songhaeunsong <gkdms3469@naver.com>
2023-04-17 22:11:53 +0900

    add:all-questions,question,ask page link

---
+++ client/src/pages/question.js
@@ -0,0 +1,4 @@
+const Question = () => {
+  return <>Question</>;
+};
+export default Question;
---
+++ client/src/pages/questionEdit.js
@@ -0,0 +1,4 @@
+const QuestionEdit = () => {
+  return <>QuestionEdit</>;
+};
+export default QuestionEdit;
