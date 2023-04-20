7801d43008d5802fba5714c733e99d4b5a334726
Choalstn <choalstn@gmail.com>
2023-04-21 01:25:39 +0900

    답글 reduc-persist 등록

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -12,7 +12,7 @@ const persistConfig = {
   // localStorage에 저장합니다.
   storage,
   // auth, board, studio 3개의 reducer 중에 auth reducer만 localstorage에 저장합니다.
-  whitelist: ['login', 'userinfo'],
+  whitelist: ['login', 'userinfo', 'answer'],
   // blacklist -> 그것만 제외합니다
 };
 
