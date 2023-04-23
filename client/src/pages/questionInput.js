1b292b50059be30fd47504f7f214259173fb57ef
songhaeunsong <gkdms3469@naver.com>
2023-04-23 16:20:35 +0900

    질문 update 완료

--- client/src/pages/questionInput.js
+++ client/src/pages/questionInput.js
@@ -2,6 +2,7 @@ import { useRef, useState, useEffect } from 'react';
 import Navigation from '../components/Navigation';
 import Container from '../components/Container.js';
 import QuestionEditor from '../components/Question/questionEditor';
+import Loading from '../components/loading';
 import '@toast-ui/editor/dist/toastui-editor.css';
 import styled from 'styled-components';
 import axios from 'axios';
@@ -15,6 +16,7 @@ const QuestionInput = () => {
   const [content, setContent] = useState('');
   const [data, setData] = useState([]);
   const [isUpdate, setIsUpdate] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
 
   const handleTitleChange = e => {
     setTitle(e.target.value);
@@ -28,12 +30,13 @@ const QuestionInput = () => {
     const instance = editorRef.current.getInstance();
     const content = instance.getMarkdown();
     e.preventDefault();
+
     if (title === '' || content === '') {
       alert('내용을 입력해주세요!');
     } else {
       axios
         .post('http://localhost:3001/QUESTION_DATA', {
-          id: data.length + 1,
+          id: data.length === 0 ? 1 : data[data.length - 1].id + 1,
           title: title,
           content: content,
           answer: [],
@@ -73,6 +76,7 @@ const QuestionInput = () => {
       setTitle('');
       setContent('');
       editorRef.current.getInstance().setMarkdown('');
+      setIsUpdate(false);
     }
   };
 
@@ -80,11 +84,13 @@ const QuestionInput = () => {
     axios.get('http://localhost:3001/QUESTION_DATA').then(res => setData(res.data));
 
     if (id) {
+      setIsLoading(true);
       setIsUpdate(true);
-      axios.get('http://localhost:3001/QUESTION_DATA/${id}').then(res => {
+      axios.get(`http://localhost:3001/QUESTION_DATA/${id}`).then(res => {
         setData(res.data);
         setTitle(res.data.title);
         setContent(res.data.content);
+        setIsLoading(false);
       });
     }
   }, []);
@@ -92,24 +98,28 @@ const QuestionInput = () => {
   return (
     <Container>
       <Navigation />
-      <InputDiv>
-        <PageTitle>
-          질문하기
-          <hr color="#d9d9d9" />
-        </PageTitle>
-        <QuestionEditor
-          editorRef={editorRef}
-          title={title}
-          content={content}
-          handleTitleChange={handleTitleChange}
-          handleContentChange={handleContentChange}
-        />
-        {isUpdate ? (
-          <Button onClick={handleUpdateBtn}>Update</Button>
-        ) : (
-          <Button onClick={handlePostBtn}>Post</Button>
-        )}
-      </InputDiv>
+      {isLoading ? (
+        <Loading />
+      ) : (
+        <InputDiv>
+          <PageTitle>
+            질문하기
+            <hr color="#d9d9d9" />
+          </PageTitle>
+          <QuestionEditor
+            editorRef={editorRef}
+            title={title}
+            content={content}
+            handleTitleChange={handleTitleChange}
+            handleContentChange={handleContentChange}
+          />
+          {isUpdate ? (
+            <Button onClick={handleUpdateBtn}>Update</Button>
+          ) : (
+            <Button onClick={handlePostBtn}>Post</Button>
+          )}
+        </InputDiv>
+      )}
     </Container>
   );
 };
