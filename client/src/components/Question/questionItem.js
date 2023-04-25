5e91393f57c130e04cd3b7bc5f7951aaf7edf82f
songhaeunsong <gkdms3469@naver.com>
2023-04-26 02:15:51 +0900

    add: search 기능

--- client/src/components/Question/questionItem.js
+++ client/src/components/Question/questionItem.js
@@ -1,40 +1,33 @@
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 
-const QuestionItem = ({ questions }) => {
+const QuestionItem = ({ question }) => {
   return (
     <div>
-      {questions.map((question, index) => (
-        <ItemDiv key={index}>
-          <Item>
-            <Response>
-              <p>0 votes</p>
-              <p>0 answer</p>
-              <p>0 views</p>
-            </Response>
-            <Question>
-              {/* 실제 서버 연결용 */}
-              {/* <Link to={`/questions/${question.questionId}`}> */}
-              <Link to={`/questions/${question.id}`}>
-                <h3>{question.title}</h3>
-              </Link>
-              <content>{question.content}</content>
-            </Question>
-          </Item>
-        </ItemDiv>
-      ))}
+      <Item>
+        <Response>
+          <p>0 votes</p>
+          <p>0 answer</p>
+          <p>0 views</p>
+        </Response>
+        <Question>
+          {/* 실제 서버 연결용 */}
+          {/* <Link to={`/questions/${question.questionId}`}> */}
+          <Link to={`/questions/${question.id}`}>
+            <h3>{question.title}</h3>
+          </Link>
+          <content>{question.content}</content>
+        </Question>
+      </Item>
     </div>
   );
 };
-
-const ItemDiv = styled.div`
-  border-bottom: 2px solid #d9d9d9;
-`;
 
 const Item = styled.div`
   padding: 2rem;
   display: flex;
   font-size: 1.2rem;
+  border-bottom: 2px solid #d9d9d9;
 `;
 const Question = styled.div`
   width: 80%;
